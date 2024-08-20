e04085b40a39dd6b339ae685644a4be5f47e3b05
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-20 17:05:35 +0200

    add structure for Meteor component

---
+++ src/components/Meteor/Meteor.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Meteor.module.scss'
+
+export const Meteor: React.FC = () => {
+    return (
+        <div className={styles.test}>
+            Meteor
+        </div>
+    )
+}
