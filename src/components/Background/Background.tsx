14b3d2435e95e59489b1d79926e19babdc02c3c5
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-21 21:38:17 +0200

    add scrolling background

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import styles from './Background.module.scss';
 
 export const Background: React.FC = () => {
-  console.log('bcg');
+  const BCGs = [1]; //FIXED LENGTH TO CHANGE
+
   return (
     <div className={styles.container}>
-      <div className={styles.slider}></div>
+      <div
+        className={styles.slideWrapper}
+        style={{ width: `${BCGs.length * 16000}px` }}
+      >
+        {BCGs.map((_, index) => (
+          <div className={styles.slide} key={index}></div>
+        ))}
+      </div>
     </div>
   );
 };
