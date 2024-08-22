e3f06c3004b10bb26de33dadf4ebb05757da6d58
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-22 21:30:03 +0200

    fix background animation

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import styles from './Background.module.scss';
 
-export const Background: React.FC = () => {
-  const BCGs = [1]; //FIXED LENGTH TO CHANGE
+type BackgroundPropos = {
+  gameStatus: 'GameStart' | 'GameActive' | 'GameWon' | 'GameOver';
+};
+
+export const Background: React.FC<BackgroundPropos> = ({ gameStatus }) => {
+  const BCGs = [1]; //FIXED LENGTH TO CHANGE - should depend on level
+
+  let statusForStyling;
+
+  gameStatus === 'GameActive'
+    ? (statusForStyling = true)
+    : (statusForStyling = false);
 
   return (
     <div className={styles.container}>
       <div className={styles.panel} />
       <div
-        className={styles.slideWrapper}
-        style={{ width: `${BCGs.length * 16000}px` }}
+        className={`${styles.slideWrapper} ${gameStatus === 'GameActive' ? styles.GameActive : styles.GameInactive}`}
+        style={{ width: `${BCGs.length * 8000}px` }}
       >
         {BCGs.map((_, index) => (
           <div className={styles.slide} key={index}></div>
