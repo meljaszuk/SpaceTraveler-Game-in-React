d67cc3654441a4a0d7e3e0908a2c1465c992348f
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-26 00:52:54 +0200

    add game Grid

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -4,8 +4,20 @@ import styles from './Game.module.scss';
 export const Game: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>space traveller</h1>
-      <div className={styles.button} />
+      <div className={styles.gameScreen}>
+        <h1 className={styles.title}>space traveller</h1>
+        <div className={styles.button} />
+      </div>
+      <div className={styles.gamePanel}>
+        <div className={styles.gameBullets}>
+          <div className={styles.bulletIcon}></div>
+          <div className={styles.bulletIcon}></div>
+          <div className={styles.bulletIcon}></div>
+          <div className={styles.bulletIcon}></div>
+        </div>
+        <div className={styles.gameLevel}>LEVEL 1</div>
+        <div className={styles.gameScore}>03567</div>
+      </div>
     </div>
   );
 };
