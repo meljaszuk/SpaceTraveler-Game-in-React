bd0f3d2a068a2b394cd12c37f45b6ff7e3df2c9f
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-30 22:56:52 +0200

    create GameStart component

---
+++ src/components/GameStart/GameStart.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './GameStart.module.scss';
+import { Level } from '../Level';
+import { Bullets } from '../Bullets';
+import { Score } from '../Score';
+
+export const GameStart: React.FC = () => {
+  const handleNotReady = () => {
+    alert('Game is still in development...');
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.gameScreen}>
+        <h1 className={styles.title}>space traveller</h1>
+        <div className={styles.button} onClick={handleNotReady} />
+      </div>
+      <div className={styles.gamePanel}>
+        <Bullets />
+        <Level />
+        <Score />
+      </div>
+    </div>
+  );
+};
