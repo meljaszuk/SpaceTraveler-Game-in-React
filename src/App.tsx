fc905aef005c7830ce405eb894fa29cc61f9b246
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-30 22:58:53 +0200

    add temporaty links to Game-state components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Background } from './components/Background';
-import { Game } from './components/Game';
+import { GameStart } from './components/GameStart';
+import { GameActive } from './components/GameActive';
+import { GameOver } from './components/GameOver';
+import { GameWon } from './components/GameWon';
 import styles from './App.module.scss';
 
 export const App: React.FC = () => {
@@ -10,10 +13,26 @@ export const App: React.FC = () => {
 
   console.log('app launched');
 
+  const handleOnClick = (gameStat: 'GameStart' | 'GameActive' | 'GameWon' | 'GameOver') => {
+    setGameStatus(gameStat);
+  }
   return (
-    <div className={styles.body}>
-      <Background gameStatus={gameStatus} />
-      <Game />
+    <div>
+      <div className={styles.temporaryLinks}>
+        <div onClick={() => handleOnClick('GameStart')} className={styles.tempLinks}> GameStart </div>
+        <div onClick={() => handleOnClick('GameActive')} className={styles.tempLinks}> GameActive </div>
+        <div onClick={() => handleOnClick('GameWon')} className={styles.tempLinks}> GameWon </div>
+        <div onClick={() => handleOnClick('GameOver')} className={styles.tempLinks}> GameOver </div>
+      </div>
+      
+      <div className={styles.body}>
+        <Background gameStatus={gameStatus} />
+        {gameStatus === 'GameStart' && <GameStart />}
+        {gameStatus === 'GameActive' && <GameActive />}
+        {gameStatus === 'GameWon' && <GameWon />}
+        {gameStatus === 'GameOver' && <GameOver />}
+      </div>
     </div>
+
   );
 };
