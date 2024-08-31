770dbce8b807abbcde36ada9587ac79aae61cee5
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-31 22:22:48 +0200

    remove Title from GameActive

--- src/components/GameActive/GameActive.tsx
+++ src/components/GameActive/GameActive.tsx
@@ -3,7 +3,6 @@ import styles from './GameActive.module.scss';
 import { Level } from '../Level';
 import { Bullets } from '../Bullets';
 import { Score } from '../Score';
-import { Title } from '../Title';
 
 export const GameActive: React.FC = () => {
   const handleNotReady = () => {
@@ -12,7 +11,6 @@ export const GameActive: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.gameScreen}>
-        <Title />
         <div>GameActive in progress</div>
       </div>
       <div className={styles.gamePanel}>
