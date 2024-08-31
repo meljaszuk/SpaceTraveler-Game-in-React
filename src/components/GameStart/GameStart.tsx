8c9b6787346b56db4fbe3c2b19ccc46470b5667d
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-31 22:23:19 +0200

    add Context import and use

--- src/components/GameStart/GameStart.tsx
+++ src/components/GameStart/GameStart.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './GameStart.module.scss';
 import { Level } from '../Level';
 import { Bullets } from '../Bullets';
 import { Score } from '../Score';
 import { Title } from '../Title';
-/* import { GameState } from '../../types/types'; */
+import { AppContext } from '../../context';
 
 export const GameStart: React.FC = () => {
-  const handleNotReady = () => {
-    alert('Feature in progress');
+
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error('AppContext must be used within a ContextProvider');
+  }
+
+  const {setGameStatus} = context;
+
+  const handleStart = () => {
+    setGameStatus('GameActive');
   };
   return (
     <div className={styles.container}>
       <div className={styles.gameScreen}>
         <Title />
-        <div className={styles.button} onClick={handleNotReady} />
+        <div className={styles.button} onClick={handleStart} />
       </div>
       <div className={styles.gamePanel}>
         <Bullets />
