589ac2b2879602b00f5c308b81d3c647e8646a53
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-31 22:24:30 +0200

    import and use Context to style Title

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Title.module.scss';
+import { AppContext } from '../../context';
 
 export const Title: React.FC = () => {
-  return <h1 className={styles.title}>space traveller</h1>;
+
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error('AppContext must be used within a ContextProvider');
+  }
+
+  const {gameStatus} = context;
+
+  return (
+    <div>
+      {gameStatus === 'GameStart' && <div className={`${styles.GameStart} ${styles.general}`}>space traveller</div>}
+      {gameStatus === 'GameActive' && <div></div>}
+      {gameStatus === 'GameWon' && <div className={`${styles.GameWon} ${styles.general}`}>great job!</div>}
+      {gameStatus === 'GameOver' && <div className={`${styles.GameOver} ${styles.general}`}>game over</div>}
+    </div>
+
+  );
 };
