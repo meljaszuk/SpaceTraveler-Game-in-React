da497667f3c1eca42088e7dd69def532d36dc69a
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-01 19:20:14 +0200

    add Loader animation

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,17 +3,30 @@ import styles from './Loader.module.scss';
 
 export const Loader: React.FC = () => {
 
-    useEffect(()=>{
-        console.log('Loader done')
-    },[])
+    useEffect(() => {
+        console.log('Loader done');
+    }, []);
+
     return (
         <div className={styles.wrapper}>
-            LOADER!!!!
-            <div className={styles.sun}></div>
-            <div className={styles.planet1}></div>
-            <div className={styles.planet2}></div>
-            <div className={styles.planet3}></div>
-            <div className={styles.planet4}></div>
+            <div className={styles.animation}>
+                <div className={styles.sun}></div>
+                <div className={styles.planet1}>
+                    <div className={styles.circle1}></div>
+                </div>
+                <div className={styles.planet2}>
+                    <div className={styles.circle2}></div>
+                </div>
+                <div className={styles.planet3}>
+                    <div className={styles.circle3}></div>
+                </div>
+                <div className={styles.planet4}>
+                    <div className={styles.circle4}></div>
+                </div>
+            </div>
+            <div className={styles.title}>
+                LOADING
+            </div>
         </div>
-    )
-}
+    );
+};
