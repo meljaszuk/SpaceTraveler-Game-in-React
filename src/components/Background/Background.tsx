4c59d5352337337d20d0197087971dce213dba37
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-01 20:27:08 +0200

    pause bcg scrolling during counting down

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styles from './Background.module.scss';
+import { AppContext } from '../../context';
 
-type BackgroundPropos = {
-  gameStatus: 'GameStart' | 'GameActive' | 'GameWon' | 'GameOver';
-};
-
-export const Background: React.FC<BackgroundPropos> = ({ gameStatus }) => {
+export const Background: React.FC = () => {
   const BCGs = [1]; //FIXED LENGTH TO CHANGE - should depend on level
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error('AppContext must be used within a ContextProvider');
+  }
+
+  const { gameStatus, isPaused } = context;
 
   let statusForStyling;
 
@@ -18,7 +22,7 @@ export const Background: React.FC<BackgroundPropos> = ({ gameStatus }) => {
     <div className={styles.container}>
       <div className={styles.panel} />
       <div
-        className={`${styles.slideWrapper} ${gameStatus === 'GameActive' ? styles.GameActive : styles.GameInactive}`}
+        className={`${styles.slideWrapper} ${gameStatus === 'GameActive' ? styles.GameActive : styles.GameInactive} ${isPaused ? styles.GamePaused : ""}`}
         style={{ width: `${BCGs.length * 8000}px` }}
       >
         {BCGs.map((_, index) => (
