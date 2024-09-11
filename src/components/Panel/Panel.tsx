b593eee6cd85e93d5da57c863032b2376fb6c020
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-11 16:51:05 +0200

    add Timer to Panel

--- src/components/Panel/Panel.tsx
+++ src/components/Panel/Panel.tsx
@@ -3,6 +3,7 @@ import styles from './Panel.module.scss';
 import { Level } from '../Level';
 import { Bullets } from '../Bullets';
 import { Score } from '../Score';
+import { Timer } from '../Timer';
 import { AppContext } from '../../context';
 
 export const Panel: React.FC = () => {
@@ -16,9 +17,20 @@ export const Panel: React.FC = () => {
     const { gameStatus } = context;
     return (
         <div className={`${styles.gamePanel} ${gameStatus === 'GameOver' ? styles.gameOver : styles.gameActive}`}>
+          <div className={styles.margin}></div>
+          <div className={styles.bullets}>
             <Bullets />
+          </div>
+          <div className={styles.levels}>
             <Level />
+          </div>
+          <div className={styles.timer}>
+            <Timer />
+          </div>
+          <div className={styles.score}>
             <Score />
+          </div>
+
         </div>
 )
 }
