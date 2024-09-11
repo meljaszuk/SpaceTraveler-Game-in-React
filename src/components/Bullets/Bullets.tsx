99bfb8a8c2097400da06d1687a74d26ba4d79892
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-11 13:44:25 +0200

    style bullets for GameOver

--- src/components/Bullets/Bullets.tsx
+++ src/components/Bullets/Bullets.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Bullets.module.scss';
+import { AppContext } from '../../context';
 
 export const Bullets: React.FC = () => {
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error('AppContext must be used within a ContextProvider');
+  }
+
+  const { gameStatus } = context;
   return (
     <div className={styles.gameBullets}>
-      <div className={styles.bulletIcon}></div>
-      <div className={styles.bulletIcon}></div>
-      <div className={styles.bulletIcon}></div>
-      <div className={styles.bulletIcon}></div>
+      <div className={`${styles.bulletIcon} ${gameStatus === 'GameOver' ? styles.gameOver : styles.gameActive}`}></div>
+      <div className={`${styles.bulletIcon} ${gameStatus === 'GameOver' ? styles.gameOver : styles.gameActive}`}></div>
+      <div className={`${styles.bulletIcon} ${gameStatus === 'GameOver' ? styles.gameOver : styles.gameActive}`}></div>
+      <div className={`${styles.bulletIcon} ${gameStatus === 'GameOver' ? styles.gameOver : styles.gameActive}`}></div>
     </div>
   );
 };
