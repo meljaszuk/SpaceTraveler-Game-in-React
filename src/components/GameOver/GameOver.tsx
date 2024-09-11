542d90d902b57311ec993231c818ba1528d19623
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-11 13:21:36 +0200

    apply Panel component

--- src/components/GameOver/GameOver.tsx
+++ src/components/GameOver/GameOver.tsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import styles from './GameOver.module.scss';
-import { Level } from '../Level';
-import { Bullets } from '../Bullets';
-import { Score } from '../Score';
 import { Title } from '../Title';
 import { Loader } from '../Loader';
+import { StartButton } from '../StartButton';
+import { Panel } from '../Panel';
+
 
 export const GameOver: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [isExplostion, setIsExplostion] = useState<boolean>(true)
   const [isScreenHidden, setIsScreenHidden] = useState<boolean>(true)
 
   useEffect(() => {
@@ -17,22 +16,25 @@ export const GameOver: React.FC = () => {
   }, []);
 
   useEffect (() => {
-    setTimeout(()=> setIsExplostion(false), 1000)
-  }, [])
-
-  useEffect (() => {
     setTimeout(()=> setIsScreenHidden(false), 800)
   }, [])
 
   return (
     <div className={styles.container}>
-      <div className={`${isExplostion ? styles.explosion : styles.explosionDone}`}></div>
       {isLoading ? (
         <Loader />
       ) : (
         <>        
-          <div className={`${isScreenHidden ? styles.gameScreenHidden : styles.gameScreen}`}></div>
-          <div className={styles.gamePanel}></div>
+
+          <div className={styles.videoContainer}>
+            <Title />
+            <StartButton />
+            <video autoPlay loop className={styles.backgroundVideo}>
+              <source src={require('./game-over-video.mp4')} type="video/mp4" />
+              Your brwoser doesn't support videos.
+            </video>
+            <Panel />
+          </div>
         </>
       )}
     </div>
