c13e7a5dec8840203cdabe937da4ab14b6ada2d1
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-11 13:21:59 +0200

    aplly Panel component

--- src/components/GameWon/GameWon.tsx
+++ src/components/GameWon/GameWon.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styles from './GameWon.module.scss';
-import { Level } from '../Level';
-import { Bullets } from '../Bullets';
-import { Score } from '../Score';
+import { Panel } from '../Panel';
 import { Title } from '../Title';
 import { Loader } from '../Loader';
 
@@ -27,11 +25,7 @@ export const GameWon: React.FC = () => {
             <Title />
             <div>GameWon in progress</div>
           </div>
-          <div className={styles.gamePanel}>
-            <Bullets />
-            <Level />
-            <Score />
-          </div>
+          <Panel />
         </>
       )}
       ;
