fd3b57f7aa79013a86d5df35957a6a8c4ed710b1
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-11 13:21:02 +0200

    apply panel component

--- src/components/GameStart/GameStart.tsx
+++ src/components/GameStart/GameStart.tsx
@@ -1,28 +1,15 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './GameStart.module.scss';
-import { Level } from '../Level';
-import { Bullets } from '../Bullets';
-import { Score } from '../Score';
+import { Panel } from '../Panel';
 import { Title } from '../Title';
-import { AppContext } from '../../context';
 import { Loader } from '../Loader';
+import { StartButton } from '../StartButton';
 
 export const GameStart: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const context = useContext(AppContext);
-
-  if (!context) {
-    throw new Error('AppContext must be used within a ContextProvider');
-  }
-
-  const { setGameStatus } = context;
-
-  const handleStart = () => {
-    setGameStatus('GameActive');
-  };
 
   useEffect(() => {
-    setTimeout(() => setIsLoading(false), 2000); /* LOADING SIMULATION */
+    setTimeout(() => setIsLoading(false), 500); /* LOADING SIMULATION */
     console.log('LOADER done');
   }, []);
 
@@ -34,14 +21,10 @@ export const GameStart: React.FC = () => {
         <>
           <div className={styles.gameScreen}>
             <Title />
-            <div className={styles.button} onClick={handleStart} />
+            <StartButton />
             {/*   ADD INSTRUCTIONS */}
           </div>
-          <div className={styles.gamePanel}>
-            <Bullets />
-            <Level />
-            <Score />
-          </div>
+          <Panel />
         </>
       )}
     </div>
