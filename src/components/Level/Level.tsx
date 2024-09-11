91a6bdf15c5b11bdcc3dc1fea17eee7d10f82a7c
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-11 14:02:34 +0200

    style Level for GameOver

--- src/components/Level/Level.tsx
+++ src/components/Level/Level.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Level.module.scss';
+import { AppContext } from '../../context';
 
 export const Level: React.FC = () => {
-  return <div className={styles.gameLevel}>LEVEL 1</div>;
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error('AppContext must be used within a ContextProvider');
+  }
+
+  const { gameStatus } = context;
+  return (
+    <div className={styles.gameLevel}>
+      <div className={styles.wrapper}>
+        L<em className={`${styles.gameLevel} ${gameStatus === 'GameOver' ? styles.gameOver : styles.gameActive}`}>EV</em>EL_|
+      </div>   
+  </div>
+  );
 };
