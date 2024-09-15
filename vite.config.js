5ce35340b6050a2f036194e78e0a9ce1f3a00a4f
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-15 11:08:13 +0200

    add base to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/SpaceTraveler-Game-in-React/', // Ustaw bazowy URL zgodnie z nazwą repozytorium
 });
