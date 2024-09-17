9f940da2970644b8d4871348f415d72903c0df25
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-17 15:18:13 +0200

    adjust style fro GameOver state

--- src/components/InfoButton/InfoButton.tsx
+++ src/components/InfoButton/InfoButton.tsx
@@ -12,7 +12,7 @@ export const InfoButton: React.FC = () => {
   const { gameStatus } = context;
   return (
     <div className={styles.container}>
-      <div className={styles.info}>INFO</div>
+      <div className={`${styles.info} ${gameStatus === 'GameOver' ? styles.GameOver : ""}`}>INFO</div>
     </div>
   );
 };
