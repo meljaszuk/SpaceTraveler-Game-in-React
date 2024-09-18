230c1ab1b034241f336f434960d2d7cfcc4ea426
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-18 10:56:59 +0200

    create Info component

---
+++ src/components/Info/Info.tsx
@@ -0,0 +1,50 @@
+import React, { useContext } from 'react';
+import styles from './Info.module.scss';
+import { AppContext } from '../../context';
+
+export const Info: React.FC = () => {
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error('AppContext must be used within a ContextProvider');
+  }
+
+  const { isInfo, setIsInfo } = context;
+
+  const handleInfo = () => {
+    setIsInfo(false)
+  }
+
+  return (
+    <>
+    {isInfo && <div className={styles.container}>
+      <div className={styles.subContainer}>
+          <div className={styles.close} onClick={handleInfo}>
+            <div className={styles.cross}>x</div>
+          </div>
+          <div className={styles.info}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempora quod voluptates velit eum pariatur dicta nobis soluta quos accusamus, tenetur accusantium ducimus consequuntur cupiditate quasi hic. Est, magni beatae!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat vel animi dicta, aluam nam velit iure! At ab ratione ullam, veritatis eveniet commodi unde architecto, alias esse earum quam possimus!
+          </div>
+          <div className={styles.info}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempora quod voluptates velit eum pariatur dicta nobis soluta quos accusamus, tenetur accusantium ducimus consequuntur cupiditate quasi hic. Est, magni beatae!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat vel animi dicta, aluam nam velit iure! At ab ratione ullam, veritatis eveniet commodi unde architecto, alias esse earum quam possimus!
+          </div>
+          <div className={styles.info}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempora quod voluptates velit eum pariatur dicta nobis soluta quos accusamus, tenetur accusantium ducimus consequuntur cupiditate quasi hic. Est, magni beatae!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat vel animi dicta, aluam nam velit iure! At ab ratione ullam, veritatis eveniet commodi unde architecto, alias esse earum quam possimus!
+          </div>
+          <div className={styles.info}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempora quod voluptates velit eum pariatur dicta nobis soluta quos accusamus, tenetur accusantium ducimus consequuntur cupiditate quasi hic. Est, magni beatae!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat vel animi dicta, aluam nam velit iure! At ab ratione ullam, veritatis eveniet commodi unde architecto, alias esse earum quam possimus!
+          </div>
+          <div className={styles.info}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempora quod voluptates velit eum pariatur dicta nobis soluta quos accusamus, tenetur accusantium ducimus consequuntur cupiditate quasi hic. Est, magni beatae!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat vel animi dicta, aluam nam velit iure! At ab ratione ullam, veritatis eveniet commodi unde architecto, alias esse earum quam possimus!
+          </div>
+      </div>
+    </div>
+    }
+    </>
+  );
+};
