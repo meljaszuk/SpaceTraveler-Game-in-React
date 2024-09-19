0a69c608ec1c7b50333764057ec9069b30b3c18a
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-19 12:35:55 +0200

    update gameConfig constnts

--- src/gameConfig/gameConfig.tsx
+++ src/gameConfig/gameConfig.tsx
@@ -6,35 +6,48 @@ export const randomBcg = (): number => Math.floor(Math.random() * 5) + 1;
 export const meteors: Meteor[] = [
   {
     id: 1,
-    size: "s",
-    x: 50,
-    y: 50,
+    size: 30,
+    x: 920,
+    y: 10,
     bcg: randomBcg(), 
     timeOut: 0,
     rotation: randomRotation(), 
   },
   {
     id: 2,
-    size: "m",
-    x: 70,
-    y: 70,
+    size: 50,
+    x: 1020,
+    y: 410,
     bcg: randomBcg(), 
     timeOut: 1000,
     rotation: randomRotation(), 
   },
   {
     id: 3,
-    size: "l",
-    x: 20,
-    y: 20,
+    size: 90,
+    x: 720,
+    y: 90,
     bcg: randomBcg(), 
     timeOut: 2000,
     rotation: randomRotation(), 
-  }
+  },
+  {
+    id: 4,
+    size: 100,
+    x: 920,
+    y: 250,
+    bcg: randomBcg(), 
+    timeOut: 2000,
+    rotation: randomRotation(), 
+  },
+  {
+    id: 5,
+    size: 70,
+    x: 820,
+    y: 350,
+    bcg: randomBcg(), 
+    timeOut: 2000,
+    rotation: randomRotation(), 
+  },
 ]
 
-export const scores: Record<"s" | "m" | "l", number> = {
-    "s": 500,
-    "m": 300,
-    "l": 100
-}
