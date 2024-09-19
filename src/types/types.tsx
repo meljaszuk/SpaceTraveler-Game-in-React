a75e436e0d40a6d5161fb6f387e03e9bf7dc0958
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-19 11:06:04 +0200

    add Meteor interface

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,1 +1,12 @@
 export type GameState = 'GameStart' | 'GameActive' | 'GameWon' | 'GameOver';
+
+export interface Meteor {
+    id: number;
+    size: "s" | "m" | "l";
+    x: number;
+    y: number;
+    bcg: number
+    timeOut: number;
+    rotation: number
+}
+
