aeb1395f573abfdc749e4ec50c31eb645cf84490
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-19 12:35:28 +0200

    create PlayGame component

--- src/components/PlayGame/PlayGame.tsx
+++ src/components/PlayGame/PlayGame.tsx
@@ -9,15 +9,36 @@ export const PlayGame: React.FC = () => {
       throw new Error('AppContext must be used within a ContextProvider');
     }
   
-    const { meteors, randomBcg, randomRotation, scores } = context;
+    const { meteors } = context;
 
     useEffect(() => {
         for(let meteor in meteors) {
-            
+
         }
     },[])
 
     return (
-        <div className={styles.playground}>x</div>
+
+   <div className={styles.playground}>
+            <div className={styles.wrapper}>
+            {meteors.map((item, index) => (
+        <div
+          key={index}
+          className={styles.meteor}
+          style={{
+            top: `${item.y}px`,
+            left: `${item.x}px`,
+            width: `${item.size}px`,
+            height: `${item.size}px`,
+            transform: `rotate(${item.rotation}deg)`,
+          }}
+        >
+          {item.id}
+        </div>
+            ))} 
+            </div>
+        </div>
+   
+     
     )
     }
