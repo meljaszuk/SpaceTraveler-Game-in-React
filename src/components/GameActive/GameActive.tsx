61aae2fc726c234f04afdc31fa41d60c77265799
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-20 14:58:43 +0200

    add feature: end level after time set in config

--- src/components/GameActive/GameActive.tsx
+++ src/components/GameActive/GameActive.tsx
@@ -1,18 +1,45 @@
-import React, { useState, useEffect /* , useContext */ } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import styles from './GameActive.module.scss';
 import { Panel } from '../Panel';
 import { Loader } from '../Loader';
 import { Counter } from '../Counter';
 import { PlayGame } from '../PlayGame';
-/* import { AppContext } from '../../context'; */
+import { AppContext } from '../../context';
 
 export const GameActive: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error('AppContext must be used within a ContextProvider');
+  }
+
+  const { TIME_PER_LEVEL, setTime, setGameStatus, time, isPaused} = context;
 
   useEffect(() => {
     setIsLoading(false);
-    console.log('LOADER done');
+    setTime(TIME_PER_LEVEL)
   }, []);
+
+  useEffect(() => {
+    if(time >= 0 && !isPaused) {
+      setTimeout(()=> {
+      setTime(time - 1)
+    }, 1000)
+  }
+  }, [time, isPaused])
+
+  useEffect(() => {
+    if(time < 0) {
+      setGameStatus('GameWon')
+      setTime(TIME_PER_LEVEL)
+    }
+
+  }, [time])
+
+  useEffect(()=> {
+    console.log(time)
+  }, [time])
 
   return (
     <div className={styles.container}>
