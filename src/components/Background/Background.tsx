6a6a79f79e0ac310f2dad26074e250c996291c03
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-24 13:00:50 +0200

    make bcg scrolling time dependent on globl const

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -10,13 +10,13 @@ export const Background: React.FC = () => {
     throw new Error('AppContext must be used within a ContextProvider');
   }
 
-  const { gameStatus, isPaused } = context;
+  const { gameStatus, isPaused, TIME_PER_LEVEL } = context;
 
   return (
     <div className={styles.container}>
       <div
         className={`${styles.slideWrapper} ${gameStatus === 'GameActive' ? styles.GameActive : gameStatus === 'GameOver' ? styles.GameOver : styles.GameInactive} ${isPaused ? styles.GamePaused : ''}`}
-        style={{ width: `${BCGs.length * 8000}px` }}
+        style={{ width: `${BCGs.length * 8000}px`, animationDuration: `${TIME_PER_LEVEL + 1}s` }}
       >
         {BCGs.map((_, index) => (
           <div className={styles.slide} key={index}></div>
